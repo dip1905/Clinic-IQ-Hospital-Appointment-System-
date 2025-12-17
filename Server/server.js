170b672770b08b75fbfab8af32820d461555c672
dip1905 <dipesh.patel9977@gmail.com>
2025-12-17 12:58:42 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const path = require('path'); 
+// const path = require('path'); 
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const appointmentRoutes = require('./routes/appointmentRoutes');
@@ -15,14 +15,14 @@ dotenv.config();
 connectDB();
 
 const app = express();
-// app.use(cors({
-//   origin: 'https://clinic-iq-hospital-appointment-system.onrender.com',
-//   credentials: true
-// }));
 app.use(cors({
-  origin: true,
+  origin: 'https://clinic-iq-hospital-appointment-system.onrender.com',
   credentials: true
 }));
+// app.use(cors({
+//   origin: true,
+//   credentials: true
+// }));
 
 app.use(cookieParser());
 app.use(express.json());
@@ -33,11 +33,11 @@ app.use('/api', doctorRoutes);
 app.use('/api', appointmentRoutes);
 app.use('/api', adminRoutes);
 
-app.use(express.static(path.join(__dirname, "../client/build")));
+// app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+// app.get("/*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
