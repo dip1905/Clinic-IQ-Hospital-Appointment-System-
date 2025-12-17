11c9732356866eaf407a02443fb01b71e1d118ae
dip1905 <dipesh.patel9977@gmail.com>
2025-12-17 22:46:43 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Register from './pages/Register';
