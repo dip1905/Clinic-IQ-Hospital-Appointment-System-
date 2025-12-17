2ff7eff33d73e1e82e39b69982f7cb8fd0ef92bb
dip1905 <dipesh.patel9977@gmail.com>
2025-12-17 21:45:40 +0530

    Update Login.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { login } from '../services/api';
 import { useNavigate } from 'react-router-dom';
 import '../css/Login.css';
